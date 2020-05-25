9316669ec5ca725e6ffe472ffec35400dde395f7
stefian <stefan.ianta@w3ai.net>
2020-05-25 13:17:48 -0300

    + res.setHeader(Access-Control-Allow-Methods, GET,

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
 });
 
 app.use('/feed', feedRoutes);
