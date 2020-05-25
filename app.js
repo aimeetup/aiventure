f6d2356ab8a458758135d402bcc563ed19a63883
stefian <stefan.ianta@w3ai.net>
2020-05-25 10:30:51 -0300

    + app.use(bodyParser.json());

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json 
 
 app.use('/feed', feedRoutes);
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,18 @@
-exports.getPosts = (req, res, next) => { };
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        posts: [{
+            title: 'First post',
+            content: 'This is the first post!'
+        }]
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    // Create post in db
+    res.status(201).json({  // 201 = Success: Resource was created
+        message: 'Post created successfully!',
+        post: { id: new Date().toISOString(), title: title, content: content }
+    });
+};
