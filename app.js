62b3bd84f37b48246b159f30e161bd1905d48656
stefian <stefan.ianta@w3ai.net>
2020-05-25 12:57:40 -0300

    + res.setHeader(Access-Control-Allow-Origin, *);

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
