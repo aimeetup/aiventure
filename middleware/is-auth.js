cd173a454f879531f410b266e440e237267fccf0
stefian <stefan.ianta@w3ai.net>
2020-05-27 14:06:34 -0300

    added Authorization middleware is-auth

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-
+    const authHeader = req.get('Authorization');
+    const token = req.get('Authorization').split(' ')[1];   // Get the 2nd value after Bearer
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, `${process.env.JWT_SECRET}`);
+    } catch (err) {
+        err.statusCode = 500;
+        throw err;
+    }
+    if (!decodedToken) {
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+    req.userId = decodedToken.userId;
+    next();
 };
