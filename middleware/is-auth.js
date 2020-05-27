2c17942a98665a2a34b6e4df9031c88a6697e087
stefian <stefan.ianta@w3ai.net>
2020-05-27 14:10:43 -0300

    added checking for Authorization header

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,7 +2,12 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     const authHeader = req.get('Authorization');
-    const token = req.get('Authorization').split(' ')[1];   // Get the 2nd value after Bearer
+    if (!authHeader) {  // if no Authorization header (undefined)
+        const error = new Error('Not authneticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+    const token = authHeader.split(' ')[1];   // Get the 2nd value after Bearer
     let decodedToken;
     try {
         decodedToken = jwt.verify(token, `${process.env.JWT_SECRET}`);
