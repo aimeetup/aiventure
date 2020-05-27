4e682b086672388eb54940bc11623e3c963ac198
stefian <stefan.ianta@w3ai.net>
2020-05-27 10:44:15 -0300

    added pass encription and save user to db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
@@ -13,6 +14,24 @@ exports.signup = (req, res, next) => {
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
-
+    bcrypt
+        .hash(password, 12)
+        .then(hashedPw => {
+            const user = new User({
+                email: email,
+                password: hashedPw,
+                name: name
+            });
+            return user.save();
+        })
+        .then(result => {   // result of saving user in db
+            res.status(201).json({ message: 'User created!', userId: result._id });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        }); // encrypt / hash with a salt/strenght of 12
 
 };
--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
     },
     status: {
         type: String,
-        required: true
+        default: 'I am new!'     // required: true
     },
     posts: [{
         type: Schema.Types.ObjectId,
