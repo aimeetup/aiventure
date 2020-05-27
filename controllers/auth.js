d37a5f8560c67d95588f066ec5c48e5007cedb18
stefian <stefan.ianta@w3ai.net>
2020-05-27 09:53:52 -0300

    added validation for the signup auth router

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+
 
 };
