1218f3dd12bdaf24108b3b7e77db8a2534b33159
stefian <stefan.ianta@w3ai.net>
2020-05-28 09:25:08 -0300

    changed the auth signup controller to async await

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         const error = new Error('Validation failed.');
@@ -15,25 +15,22 @@ exports.signup = (req, res, next) => {
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
-    bcrypt
-        .hash(password, 12)
-        .then(hashedPw => {
-            const user = new User({
-                email: email,
-                password: hashedPw,
-                name: name
-            });
-            return user.save();
-        })
-        .then(result => {   // result of saving user in db
-            res.status(201).json({ message: 'User created!', userId: result._id });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        }); // encrypt / hash with a salt/strenght of 12
+    try {
+        const hashedPw = await bcrypt.hash(password, 12);   // encrypt / hash with a salt/strenght of 12
+
+        const user = new User({
+            email: email,
+            password: hashedPw,
+            name: name
+        });
+        const result = await user.save();   // result of saving user in db
+        res.status(201).json({ message: 'User created!', userId: result._id });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 };
 
 exports.login = (req, res, next) => {
