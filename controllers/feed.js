ca899a4fe8677a77e1938b183bd97b7a5217aebf
stefian <stefan.ianta@w3ai.net>
2020-05-28 14:54:22 -0300

    added action update for the posts channel

--- controllers/feed.js
+++ controllers/feed.js
@@ -112,13 +112,13 @@ exports.updatePost = async (req, res, next) => {
         throw error;
     }
     try {
-        const post = await Post.findById(postId);
+        const post = await (await Post.findById(postId)).populate('creator');
         if (!post) {
             const error = new Error('Could not find post.');
             error.statusCode = 404;
             throw error; // if use throw in then block => next catch will be reached
         }   // past this point we found the post in the db to update
-        if (post.creator.toString() !== req.userId) {
+        if (post.creator._id.toString() !== req.userId) {
             const error = new Error('Not authorized');
             error.statusCode = 403; // Not authorized
             throw error;
@@ -130,6 +130,7 @@ exports.updatePost = async (req, res, next) => {
         post.imageUrl = imageUrl;
         post.content = content;
         const result = await post.save(); // overwrite the old post in the db but keeping the old id
+        io.getIO().emit('posts', { action: 'update', post: result });   // To Explore options of renaming action to command / demand / offer
         res.status(200).json({ message: 'Post updated', post: result })
     } catch (err) {
         if (!err.statusCode) {  // To Refactor: ErrProc(err, 500, msg?)
