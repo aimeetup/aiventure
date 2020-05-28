f6be2de43e0131f2c5cf9f52b15df4c203e11b2a
stefian <stefan.ianta@w3ai.net>
2020-05-27 21:11:18 -0300

    added validation not Empty for user status

--- routes/auth.js
+++ routes/auth.js
@@ -33,6 +33,11 @@ router.post('/login', authController.login);
 
 router.get('/status', isAuth, authController.getUserStatus);
 
-router.patch('/status', isAuth, authController.updateUserStatus);
+router.patch('/status', isAuth, [
+    body('status')
+        .trim()
+        .not()
+        .isEmpty()
+], authController.updateUserStatus);
 
 module.exports = router;
