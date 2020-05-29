32b16e72dd4edf1e1226c0f71b408ea39336d62a
stefian <stefan.ianta@w3ai.net>
2020-05-29 15:36:32 -0300

    added a blank test framework/template for testing a controller - auth

---
+++ test/auth-controller.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
+
+describe('Auth Controller - Login', function () {
+
+});
