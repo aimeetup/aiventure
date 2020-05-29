68b5591fcf3dccf5a29d3d4ea2bad84f4db648aa
stefian <stefan.ianta@w3ai.net>
2020-05-29 06:59:00 -0300

    added a 2nd basic test

--- test/start.js
+++ test/start.js
@@ -5,3 +5,9 @@ it('should add numbers correctly', function () {
     const num2 = 3;
     expect(num1 + num2).to.equal(5);
 })
+
+it('should not give a result of 6', function () {
+    const num1 = 2;
+    const num2 = 3;
+    expect(num1 + num2).not.to.equal(6);
+})
