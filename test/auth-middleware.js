26fa314ac5d577131fb10d2a7974f7b8e4d51dcb
stefian <stefan.ianta@w3ai.net>
2020-05-29 11:49:34 -0300

    added a describe function to group the the auth middleware tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,22 +2,25 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', function () {
-    const req = {   // Create test request object
-        get: function () {
-            return null;
-        }
-    };
-    expect(authMiddleware.bind(this, req, {}, () => { })).to.throw(
-        'Not authenticated.'
-    );
-})
+describe('Auth middleware', function () {
+    it('should throw an error if no authorization header is present', function () {
+        const req = {   // Create test request object
+            get: function () {
+                return null;
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw(
+            'Not authenticated.'
+        );
+    })
 
-it('should throw an error if the authorization header is only one string', function () {
-    const req = {   // Create test request object
-        get: function () {
-            return 'xyz';
-        }
-    };
-    expect(authMiddleware.bind(this, req, {}, () => { })).to.throw();
-})
+    it('should throw an error if the authorization header is only one string', function () {
+        const req = {   // Create test request object
+            get: function () {
+                return 'xyz';
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw();
+    })
+});
+
