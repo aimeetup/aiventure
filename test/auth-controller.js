61eb71b51d87f5d1413170f7a9ca1b9c9431ad92
stefian <stefan.ianta@w3ai.net>
2020-05-29 15:53:12 -0300

    added a basic test and stub for testing the 500 error on User.findOne

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -5,5 +5,21 @@ const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
 describe('Auth Controller - Login', function () {
+    it('should throw an error with code 500 if accessing the database fails', function () {
+        sinon.stub(User, 'findOne');    // blank stub / mocking the findOne db method for User model
+        User.findOne.throws();
 
+        const req = {
+            body: {
+                email: 'test@test.com',
+                password: 'tester'
+            }
+        };
+
+        AuthController.login(req, {}, () => { }).then(result => {
+            console.log(result);
+        });
+
+        User.findOne.restore();
+    })
 });
