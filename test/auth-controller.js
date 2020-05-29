96b91ea91d8184bd0b4b6804c953b98f72179580
stefian <stefan.ianta@w3ai.net>
2020-05-29 16:09:07 -0300

    added expectation for error and statusCode 500 and calling done for async code

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
 describe('Auth Controller - Login', function () {
-    it('should throw an error with code 500 if accessing the database fails', function () {
+    it('should throw an error with code 500 if accessing the database fails', function (done) {
         sinon.stub(User, 'findOne');    // blank stub / mocking the findOne db method for User model
         User.findOne.throws();
 
@@ -17,7 +17,10 @@ describe('Auth Controller - Login', function () {
         };
 
         AuthController.login(req, {}, () => { }).then(result => {
-            console.log(result);
+            // console.log(result);
+            expect(result).to.be.an('error');   // Chai able to detect the data type including error
+            expect(result).to.have.property('statusCode', 500);
+            done();
         });
 
         User.findOne.restore();
