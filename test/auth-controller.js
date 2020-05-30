01ad3352b54c45bf617d876534d513c75403606c
stefian <stefan.ianta@w3ai.net>
2020-05-30 09:55:52 -0300

    added before and after hooks

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -10,7 +10,26 @@ const AuthController = require('../controllers/auth');
 const MONGODB_URI =
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-n7ze3.mongodb.net/test-aipages`;
 
-describe('Auth Controller - Login', function () {
+describe('Auth Controller', function () {
+    before(function (done) {    // before hook - initialization script - executes once before all test cases
+        mongoose
+            .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+            .then(result => {
+                // const server = app.listen(process.env.PORT || 8080);   
+                const user = new User({
+                    email: 'test@test.com',
+                    password: 'tester',
+                    name: 'Test',
+                    posts: [],
+                    _id: '5c0f66b979af55031b34728a'
+                });
+                return user.save();
+            })
+            .then(() => {
+                done();
+            })
+    })
+
     it('should throw an error with code 500 if accessing the database fails', function (done) {
         sinon.stub(User, 'findOne');    // blank stub / mocking the findOne db method for User model
         User.findOne.throws();
@@ -33,47 +52,36 @@ describe('Auth Controller - Login', function () {
     });
 
     it('should send a response with a valid user status for an existing user', function (done) {
-        mongoose
-            .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-            .then(result => {
-                // const server = app.listen(process.env.PORT || 8080);   
-                const user = new User({
-                    email: 'test@test.com',
-                    password: 'tester',
-                    name: 'Test',
-                    posts: [],
-                    _id: '5c0f66b979af55031b34728a'
-                });
-                return user.save();
+
+
+        const req = { userId: '5c0f66b979af55031b34728a' };
+        const res = {
+            statusCode: 500,
+            userStatus: null,
+            status: function (code) {
+                this.statusCode = code;
+                return this;
+            },
+            json: function (data) {
+                this.userStatus = data.status
+            }
+        };
+        AuthController.getUserStatus(req, res, () => { })
+            .then(() => {
+                expect(res.statusCode).to.be.equal(200);
+                expect(res.userStatus).to.be.equal('I am new!');
+                done();
+            })
+    })
+
+    after(function (done) {     // after hook - cleanup script
+        User.deleteMany({})
+            .then(() => {  // delete all users to have a blank db for next tests & to avoid duplicate user error
+                return mongoose.disconnect()
             })
             .then(() => {
-                const req = { userId: '5c0f66b979af55031b34728a' };
-                const res = {
-                    statusCode: 500,
-                    userStatus: null,
-                    status: function (code) {
-                        this.statusCode = code;
-                        return this;
-                    },
-                    json: function (data) {
-                        this.userStatus = data.status
-                    }
-                };
-                AuthController.getUserStatus(req, res, () => { })
-                    .then(() => {
-                        expect(res.statusCode).to.be.equal(200);
-                        expect(res.userStatus).to.be.equal('I am new!');
-                        User.deleteMany({})
-                            .then(() => {  // delete all users to have a blank db for next tests & to avoid duplicate user error
-                                return mongoose.disconnect()
-                            })
-                            .then(() => {
-                                done();
-                            });
-                    })
-            })
-            .catch(err => {
-                console.log(err);
+                done();
             });
-    })
+    });
+
 });
